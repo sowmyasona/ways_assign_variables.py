0048439ca23be0cd548caf57f743ec30fe576b39
sona@1027 <118789307+sowmyasona@users.noreply.github.com>
2022-12-06 17:52:40 +0530

    Add files via upload

---
+++ Ways_Of_Assigning_variables.py
@@ -0,0 +1,54 @@
+'''1st way of assigning values and printing them'''
+# s=7
+# G=7
+# _8=6
+# d_b=11
+# s_t_=9
+# Abc=10
+# s_t_b=2
+# print(s)
+# print(G)
+# print(_8)
+# print(d_b)
+# print(s_t_)
+# print(Abc)
+# print(s_t_b)
+
+"""2nd way of assigning values and printing them"""
+# a=5
+# A=7
+# _4=6
+# a_b=8
+# c_d_=4
+# ABC=17
+# print(a);print(A);print(_4);print(a_b);print(c_d_);print(ABC)
+# print(a),print(A),print(_4),print(a_b),print(c_d_),print(ABC)
+
+'''3rd way of assigning values and printing them'''
+# a=5;A=7;_4=6;a_b=8;c_d_=9;Abc=10
+# print(a)
+# print(A)
+# print(_4)
+# print(a_b)
+# print(c_d_)
+# print(Abc)
+
+'''4th way of assigning values and printing them'''
+# a,B,_4,a_C,c_d_=5,7,6,8,10
+# print(a)
+# print(B)
+# print(_4)
+# print(a_C)
+# print(c_d_)
+# print(PQR)
+
+'''5th way of assigning values and printing them'''
+# a=e=i=o=u=9;S=6
+# print(a)
+# print(e)
+# print(i)
+# print(o)
+# print(u)
+# print(S)
+
+# print(a,S,e,i,o,u)
